479027295baf656ecad224c735ae30f1fd9171d5
KhangDuong17 <163088497+KhangDuong17@users.noreply.github.com>
2024-03-30 15:04:02 +0700

    add file

--- model/category.php
+++ model/category.php
@@ -8,4 +8,14 @@
         $sql = "SELECT * FROM categorys WHERE id = $id";
         return get_ONE($sql);
     }
+    function category_SELECT($page,$limit) {
+        $sql = "SELECT * FROM categorys WHERE 1";
+        if ($page > 1){
+            $begin = (($page-1) * $limit);
+            $sql .="  LIMIT  $begin,$limit";
+        }else {
+            if ($limit > 0) $sql .=" LIMIT  $limit";
+        }
+        return get_All($sql);   
+    }
 ?>
--- model/publisher.php
+++ model/publisher.php
@@ -3,4 +3,14 @@
         $sql = "SELECT * FROM publishers WHERE id = $id";
         return get_ONE($sql);
     }
+    function publisher_SELECT($page,$limit) {
+        $sql = "SELECT * FROM publishers WHERE 1";
+        if ($page > 1){
+            $begin = (($page-1) * $limit);
+            $sql .="  LIMIT  $begin,$limit";
+        }else {
+            if ($limit > 0) $sql .=" LIMIT  $limit";
+        }
+        return get_All($sql);   
+    }
 ?>
