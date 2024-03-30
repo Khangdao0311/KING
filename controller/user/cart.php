45e43a87d912c386dd577d2b85c893676834f6f8
HungNguyen305 <hungnguyen305204@gmail.com>
2024-03-30 23:09:02 +0700

    update page cart

--- controller/user/cart.php
+++ controller/user/cart.php
@@ -21,10 +21,41 @@
                         $_SESSION['cart'][$id] = $product;
                     }
                 }
+                if(isset($_GET['id']) && $_GET['id']>0){
+                    $id = $_GET['id'];
+                    $product_cart = product_ONE($id);
+                    if (isset($_SESSION['cart'][$id])) {
+                        $quantity_cart = $_SESSION['cart'][$id]['quantity_cart'];
+                        $new_quantity = 1;
+                        if(isset($_POST['quantity_cart'])) {
+                            $new_quantity = $_POST['quantity_cart'];
+                        }
+                        if ($quantity_cart + $new_quantity > 10) {
+                            $new_quantity = 10 - $quantity_cart;
+                        }
+                        $_SESSION['cart'][$id]['quantity_cart'] += $new_quantity;
+                    } else {
+                        $quantity_cart = 1;
+                        if(isset($_POST['quantity_cart'])) {
+                            $quantity_cart = $_POST['quantity_cart']; 
+                        }
+                        if ($quantity_cart > 10) {
+                            $quantity_cart = 10;
+                        }
+                        $product_cart['quantity_cart'] = $quantity_cart;
+                        $_SESSION['cart'][$id] = $product_cart;
+                    }
+                }
+                
+                
+                
                 if (isset($_GET['del'])) {
                     $del = $_GET['del'];
                     unset($_SESSION['cart'][$del]);
                 }
+                include_once 'view/user/cart.php';
+                break;
+            case 'addcart': 
                 include_once 'view/user/cart.php';
                 break;
             case 'checkout':
