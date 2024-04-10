9a6b08b8629ac8b9138d10611cd21b2c729ce4f1
HungNguyen305 <hungnguyen305204@gmail.com>
2024-04-10 15:47:10 +0700

    update

--- controller/user/cart.php
+++ controller/user/cart.php
@@ -38,73 +38,76 @@ if (isset($_GET['act'])) {
                 $check_success = '';
                 if ($_POST['voucher']) $voucher_cart =  voucher_ONE($_POST['voucher']) ;
                 if (isset($_POST['payment-button']) && $_POST['payment-button']) {
-                    $name = $_POST['name'];
-                    $email = $_POST['email'];
-                    $phone = $_POST['phone'];
-                    $province = $_POST['province'];
-                    $district = $_POST['district'];
-                    $ward = $_POST['ward'];
-                    $street = $_POST['street'];
-                    $note = $_POST['note'];
-                    $method = $_POST['method'];
-                    $voucher_id = ($_POST['voucher']) ? $_POST['voucher'] : NULL ;
-                    $address = "$province, $district, $ward, $street";
-                    $text_email = ($email) ? 'Email: <b>' . $email . '</b> <br>' : '';
-                    $content = '
-                        Tên khách hàng: <b>' . $name . '</b> <br>
-                        ' . $text_email . ' 
-                        Số điện thoại: <b>' . $phone . '</b> <br>
-                        Địa chỉ giao hàng: <b>' . $address . '</b> <br>
-                        Ghi chú: <b>' . $note . '</b> <br> <br>
-                        <table border="1">
-                            <tr>
-                                <td>STT</td>
-                                <td>id sản phẩm</td>
-                                <td>Tên sản phẩm</td>
-                                <td>Số lượng</td>
-                            </tr>';
-                    $count = 1;
-                    foreach ($_SESSION['cart'][$user_cart] as $item) {
-                        $content .= '
-                            <tr>
-                                <td>' . $count++ . '</td>
-                                <td>' . $item['id'] . '</td>
-                                <td>' . $item['name'] . '</td>
-                                <td>' . $item['quantity_cart'] . '</td>
-                            </tr>
-                        ';
-                    }
-                    $content .= '      
-                        </table>
-                    ';
-                    $mail = [
-                        [
-                            "email" => 'duan1.kingstore@gmail.com',
-                            "name" => "KING STORE",
-                        ]
-                    ];
-                    // mailer($mail,'ORDER PRODUCT',$content);
-                    $orders = order_SELECT(0, 0, 0);
-                    do {
-                        $check_code = FALSE;
-                        $random_code = rand(10000, 99999);
-                        foreach($orders as $item){
-                            if ($random_code == $item['code']) $check_code = TRUE;
-                        }
-                    } while ($check_code);
-
-                    $user_id = ($_SESSION['user'] != []) ? $_SESSION['user']['id'] : 0;
-                    order_ADD($random_code, $method, $voucher_id, $user_id);
-                    foreach ($_SESSION['cart'][$user_cart] as $item) {
-                        order_detail_ADD($item['quantity_cart'], order_ONE($random_code,0)['id'], $item['id']);
-                        product_edit($item['name'],$item['image'],$item['price'],$item['price_sale'],$item['quantity'] - $item['quantity_cart'],$item['describle'],$item['noibat'],$item['category_id'],$item['author_id'],$item['publisher_id'],$item['id']);
-                    }
-                    if ($_POST['voucher']) {
-                        $voucher = voucher_ONE($voucher_id);
-                        voucher_eidt($voucher['code'],$voucher['price'],$voucher['start_date'],$voucher['end_date'],$voucher['quantity'] - 1,$voucher['user_id'],$voucher['id']);
-                    }
                     $check_success = 'checked';
                     if (isset($_POST['method'])) {
+                        if (isset($_POST['payment-button']) && $_POST['payment-button']) {
+                            $name = $_POST['name'];
+                            $email = $_POST['email'];
+                            $phone = $_POST['phone'];
+                            $province = $_POST['province'];
+                            $district = $_POST['district'];
+                            $ward = $_POST['ward'];
+                            $street = $_POST['street'];
+                            $note = $_POST['note'];
+                            $method = $_POST['method'];
+                            $voucher_id = ($_POST['voucher']) ? $_POST['voucher'] : NULL ;
+                            $address = "$province, $district, $ward, $street";
+                            $text_email = ($email) ? 'Email: <b>' . $email . '</b> <br>' : '';
+                            $content = '
+                                Tên khách hàng: <b>' . $name . '</b> <br>
+                                ' . $text_email . ' 
+                                Số điện thoại: <b>' . $phone . '</b> <br>
+                                Địa chỉ giao hàng: <b>' . $address . '</b> <br>
+                                Ghi chú: <b>' . $note . '</b> <br> <br>
+                                <table border="1">
+                                    <tr>
+                                        <td>STT</td>
+                                        <td>id sản phẩm</td>
+                                        <td>Tên sản phẩm</td>
+                                        <td>Số lượng</td>
+                                    </tr>';
+                            $count = 1;
+                            foreach ($_SESSION['cart'][$user_cart] as $item) {
+                                $content .= '
+                                    <tr>
+                                        <td>' . $count++ . '</td>
+                                        <td>' . $item['id'] . '</td>
+                                        <td>' . $item['name'] . '</td>
+                                        <td>' . $item['quantity_cart'] . '</td>
+                                    </tr>
+                                ';
+                            }
+                            $content .= '      
+                                </table>
+                            ';
+                            $mail = [
+                                [
+                                    "email" => 'duan1.kingstore@gmail.com',
+                                    "name" => "KING STORE",
+                                ]
+                            ];
+                            // mailer($mail,'ORDER PRODUCT',$content);
+                            $orders = order_SELECT(0, 0, 0);
+                            do {
+                                $check_code = FALSE;
+                                $random_code = rand(10000, 99999);
+                                foreach($orders as $item){
+                                    if ($random_code == $item['code']) $check_code = TRUE;
+                                }
+                            } while ($check_code);
+        
+                            $user_id = ($_SESSION['user'] != []) ? $_SESSION['user']['id'] : 0;
+                            order_ADD($random_code, $method, $voucher_id, $user_id);
+                            foreach ($_SESSION['cart'][$user_cart] as $item) {
+                                order_detail_ADD($item['quantity_cart'], order_ONE($random_code,0)['id'], $item['id']);
+                                product_edit($item['name'],$item['image'],$item['price'],$item['price_sale'],$item['quantity'] - $item['quantity_cart'],$item['describle'],$item['noibat'],$item['category_id'],$item['author_id'],$item['publisher_id'],$item['id']);
+                            }
+                            if ($_POST['voucher']) {
+                                $voucher = voucher_ONE($voucher_id);
+                                voucher_eidt($voucher['code'],$voucher['price'],$voucher['start_date'],$voucher['end_date'],$voucher['quantity'] - 1,$voucher['user_id'],$voucher['id']);
+                            }
+                            
+                        }
                         $payment_method = $_POST['method'];
                         if ($payment_method == 1) {
                             $payment_method = "Thanh toán qua Zalo Pay";
